2e3b348a97214b41c95dceaabc0fc40386df0877
tjdndrl0922@naver.com <1@DESKTOP-BDRSOA3>
2020-11-20 18:30:23 +0900

    *직각삼각형

---
+++ src/com/javaex/ex04/Ex07.java
@@ -0,0 +1,17 @@
+package com.javaex.ex04;
+
+public class Ex07 {
+	
+	public static void main(String[] args) {
+		
+		//직각삼각형
+		for(int i =1; i<=6; i++) {
+			for(int j=1; j<=i; j++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+	
+	}
+
+}
